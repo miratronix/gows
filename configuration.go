ff739246fd316e8c8238bfe71884bd98e5bc38d5
Lukas Rezek <lukas@miratronix.com>
2019-10-07 22:46:10 -0400

    Add insecure localhost functionality (#4)
    * Add insecure localhost functionality
    
    * Fix indents

--- configuration.go
+++ configuration.go
@@ -1,9 +1,12 @@
 package gows
 
 import (
+	"crypto/tls"
+	"github.com/gorilla/websocket"
 	"github.com/miratronix/logpher"
 	"math"
 	"math/rand"
+	"net/url"
 	"time"
 )
 
@@ -19,6 +22,9 @@ type Configuration struct {
 	ConnectionRetryRandomize  bool
 	HeartbeatInterval         time.Duration
 	HeartbeatMessage          []byte
+	InsecureLocalhost         bool
+
+	dialer *websocket.Dialer
 }
 
 // getRetryDuration computes the retry duration for a reconnect attempt
@@ -32,4 +38,49 @@ func (c *Configuration) getRetryDuration(attempt int) time.Duration {
 	retryInterval := int64(math.Min(random*min*math.Pow(c.ConnectionRetryFactor, float64(attempt)), max))
 
 	return time.Duration(retryInterval)
+}
+
+// getDialer gets the websocket dialer
+func (c *Configuration) getDialer() (*websocket.Dialer, error) {
+
+	// Already have a dialer, re-use it
+	if c.dialer != nil {
+		return c.dialer, nil
+	}
+
+	// Parse the URL
+	uri, err := url.Parse(c.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	// If insecure localhost is not set, we're not using wss, or we're not connecting to localhost, use the default dialer
+	if !c.InsecureLocalhost || uri.Scheme != "wss" || uri.Host != "localhost" {
+		c.dialer = websocket.DefaultDialer
+		return c.dialer, nil
+	}
+
+	// Clone the TLS configuration and set the insecure skip flag
+	tlsConfig := &tls.Config{}
+	if websocket.DefaultDialer.TLSClientConfig != nil {
+		tlsConfig = websocket.DefaultDialer.TLSClientConfig.Clone()
+	}
+	tlsConfig.InsecureSkipVerify = true
+
+	// Clone the default dialer but modify the TLS config
+	c.dialer = &websocket.Dialer{
+		NetDial:           websocket.DefaultDialer.NetDial,
+		NetDialContext:    websocket.DefaultDialer.NetDialContext,
+		Proxy:             websocket.DefaultDialer.Proxy,
+		HandshakeTimeout:  websocket.DefaultDialer.HandshakeTimeout,
+		ReadBufferSize:    websocket.DefaultDialer.ReadBufferSize,
+		WriteBufferSize:   websocket.DefaultDialer.WriteBufferSize,
+		WriteBufferPool:   websocket.DefaultDialer.WriteBufferPool,
+		Subprotocols:      websocket.DefaultDialer.Subprotocols,
+		EnableCompression: websocket.DefaultDialer.EnableCompression,
+		Jar:               websocket.DefaultDialer.Jar,
+		TLSClientConfig:   tlsConfig,
+	}
+
+	return c.dialer, nil
 }
